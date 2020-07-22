ca762c04a5f6515ce0c62017c20ca3510a654c29
sushovanAdhikari <35496666+sushovanAdhikari@users.noreply.github.com>
2020-07-21 23:29:57 -0600

    AutoMapper Added

---
+++ Dtos/CommandReadDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Commander.CommonReadDto
+{
+    public class CommandReadDto
+    {
+
+        public int Id { get; set; }
+
+
+        public string HowTo { get; set; }
+
+
+        public string Line { get; set; }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Commander.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -32,7 +33,8 @@ namespace Commander
 
             services.AddControllers();
 
-            //services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
           
         }
